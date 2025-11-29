b1b97d99342a2ff7d622a84bd1c44e7eec08bb2e
gouravsharma-00 <gourav.sharma.glamat2932@gmail.com>
2025-11-29 15:33:10 +0000

    folder restructure

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,16 +1,19 @@
-export * from './Footer'
-export * from './Hero'
-export * from './SocialProof'
-export * from './ProblemSolution'
-export * from './Features'
-export * from './AIInsights'
-export * from './ChartsPreview'
-export * from './Pricing'
+export * from './home/Footer'
+export * from './home/Hero'
+export * from './home/SocialProof'
+export * from './home/ProblemSolution'
+export * from './home/Features'
+export * from './home/AIInsights'
+export * from './home/ChartsPreview'
+export * from './home/Pricing'
+
 export * from './auth/register'
 export * from './auth/login'
-export * from './Loading'
+
 export * from './auth/security/register'
 export * from './auth/security/login'
 
 export * from './ui/fingerPrint'
+
 export * from './Model'
+export * from './Loading'
